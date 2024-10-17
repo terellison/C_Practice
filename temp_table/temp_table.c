f47a0fcf03332919f7fa7b4cab80d066fd2e41c2
terellison <ellisonterry4@gmail.com>
2024-10-16 22:27:16 -0400

    added table headers because it irked me lol

--- temp_table/temp_table.c
+++ temp_table/temp_table.c
@@ -6,10 +6,11 @@
 
 int main()
 {
+    printf("%s\t%s\n", "Fahrenheit", "Celsius");
     for(int fahr = LOWER; fahr <= UPPER; fahr += STEP)
     {
         int celsius = 5 * (fahr - 32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        printf("%d\t\t%d\n", fahr, celsius);
     }
 
     return 0;
